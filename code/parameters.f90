e697b34500f64aa77b3cb7d26d43bdc5a93d08f1
Sam Greenwood <Sam@Sams-MacBook-Pro.local>
2020-10-22 11:33:08 +0100

    Fixed inconsistency between secinyr and secinGyr

--- code/parameters.f90
+++ code/parameters.f90
@@ -7,7 +7,7 @@
     double precision, parameter :: Ev = 1.602e-19            !Electron volts / atom
     double precision, parameter :: pi = 3.1415926535897931d0
     double precision, parameter :: G  = 6.67384e-11          !Gravitational constant
-    double precision, parameter :: secingyr=3.15569e16,secinyr=pi*1e7
+    double precision, parameter :: secingyr=3.1536e16,secinyr=31536000.0
     double precision, parameter :: Rg = 8.3145
     double precision :: dt, ttot
 
